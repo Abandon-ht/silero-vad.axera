2ccd2ff3885a32836e50a8e12582aa6e67debcab
祈Inory <584688538@qq.com>
2025-01-25 06:11:55 +0800

    Add audio_forward

--- SileroOrt.py
+++ SileroOrt.py
@@ -33,15 +33,19 @@ class SileroOrt:
     def __init__(self, path: str):
         super().__init__()
 
-        batch_size = 1
+        self.batch_size = 1
         sr = 16000
-        hidden_size = 128
+        self.hidden_size = 128
         self.context_size = 64 if sr == 16000 else 32
-        self.context = np.zeros((batch_size, self.context_size), dtype=np.float32)
-        self.state = np.zeros((2, batch_size, hidden_size), dtype=np.float32)
+        self.context = np.zeros((self.batch_size, self.context_size), dtype=np.float32)
+        self.state = np.zeros((2, self.batch_size, self.hidden_size), dtype=np.float32)
         self.num_samples = 512 if sr == 16000 else 256
 
         self.model = ort.InferenceSession(path, providers=["CPUExecutionProvider"])
+
+    def reset_states(self):
+        self.context = np.zeros((self.batch_size, self.context_size), dtype=np.float32)
+        self.state = np.zeros((2, self.batch_size, self.hidden_size), dtype=np.float32)
 
     def __call__(self, x):
         data = np.concatenate([self.context, x], axis=1)
@@ -53,3 +57,20 @@ class SileroOrt:
         output, self.state = self.model.run(None, input_feed=input_feed)
         self.context = x[..., -self.context_size:]
         return output
+    
+    def audio_forward(self, x):
+        outs = []
+        self.reset_states()
+        num_samples = self.num_samples
+
+        if x.shape[1] % num_samples:
+            pad_num = num_samples - (x.shape[1] % num_samples)
+            x = np.pad(x, ((0, 0), (0, pad_num)), 'constant', value=0.0)
+
+        for i in range(0, x.shape[1], num_samples):
+            wavs_batch = x[:, i:i+num_samples]
+            out_chunk = self.__call__(wavs_batch)
+            outs.append(out_chunk)
+
+        stacked = np.concatenate(outs, axis=1)
+        return stacked
